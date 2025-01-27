7029a8886ef2946a74d0cec631a217e103cc2216
Debabratha Bhattacharya <debabratha1998@gmail.com>
2025-01-27 07:54:29 +0000

    strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,44 @@
+const name = "Deb"
+const repoCount = 1
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Debabratha-Bh-com')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-13, -12)
+// console.log(anotherString);
+
+const newStringOne = "      Deb     "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://developer.mozilla.org%20en-US%20"
+// console.log(url.replaceAll('%20', '/'));
+
+// console.log(url.includes('developer'));
+
+console.log(gameName.split('-'));
+
+ 
+
+
+
+
+
+
+
+
+
+
