e1d93e40d2ff82cf5fe71bb5c867b831b476c7e1
Debabratha Bhattacharya <debabratha1998@gmail.com>
2025-01-28 16:22:11 +0000

    scope done

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -4,10 +4,54 @@ let a =300
 if (true) {
     let a = 10
     const b = 20
-    console.log("Inner: ", a);
+    // console.log("Inner: ", a);
 }
 
 
-console.log(a);
+
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "deb"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+
+    two()
+    
+}
+
+// one()
+
+if (true) {
+    const username = "deb"
+    if (username === "deb") {
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+// ++++++++++++++++++++++++++++ Interesting +++++++++++++++++++++++++++
+console.log(addone(5));
+
+function addone(num){
+    return num + 1
+}
+
+// ReferenceError: Cannot access 'addTwo' before initialization
+// addTwo(5)
+// const addTwo = function(num){
+//     return num + 2
+// }
+
