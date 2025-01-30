6d70ff19ef1cfb0a0de1a84cdfd08cfee7fdb566
Debabratha Bhattacharya <debabratha1998@gmail.com>
2025-01-30 12:10:30 +0000

    control flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,35 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "Mar"
+
+switch (month) {
+    case "Jan":
+        console.log("January");        
+        break;
+    case "Feb":
+        console.log("Feb");        
+        break;
+    case "Mar":
+        console.log("Mar");        
+        break;
+    case "Apr":
+        console.log("Apr");        
+        break;
+    case "May":
+        console.log("May");        
+        break;
+    case "Jun":
+        console.log("Jun");        
+        break;
+
+    default:
+        console.log("Default case matched"); 
+        break;
+}
